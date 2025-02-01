1781c856ae4594473b95c7635350a420c6f485a0
pavko <paul@101.team>
2025-02-01 16:56:31 +0100

    Solve 03_reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let split = [];
+    for (i=0; i < string.length; i++) {
+        let letter = string.at(i);
+        split.unshift(letter);
+    }
+    return split.join('');    
 };
 
 // Do not edit below this line
