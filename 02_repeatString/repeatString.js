353350b808936dff574cc57ba479b27f6b4e5c9a
pavko <paul@101.team>
2025-02-01 15:52:20 +0100

    Solve 02_repeatString using loops

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(string, number) {
+    let repeat = '';
+    if (number < 0) {
+        return 'ERROR';   
+    } else {
+        for (i = 1; i <= number; i++) {
+        repeat += string;
+        }
+        return repeat;
+    }
 };
 
 // Do not edit below this line
